9518f76c7f1440addcffe6b170df87c9deedfa0c
Michael Vasiljev <gnu.xinm@gmail.com>
2026-02-21 17:45:14 +0300

    Feature/104 maximum depth of binary tree (#37)
    * feat: [LeetCode #104] Maximum Depth of Binary Tree
    
    Тип: Binary Tree
    Сложность: easy
    Временная сложность: O(n)
    Пространственная сложность: O(1)
    
    -
    
    Ссылка: https://leetcode.com/problems/maximum-depth-of-binary-tree/
    
    * fix: black reformatting

---
+++ structures/__init__.py
@@ -0,0 +1,3 @@
+from .tree import TreeNode
+
+__all__ = ["TreeNode"]
---
+++ structures/tree.py
@@ -0,0 +1,5 @@
+class TreeNode:
+    def __init__(self, val: int = 0, left: int | None = None, right: int | None = None):
+        self.val = val
+        self.left = left
+        self.right = right
